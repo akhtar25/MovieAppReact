ef84d66684b6faff5f62a6ba74eeee168a5f321f
Zaheen Akhtar <zaheen@alllearn.in>
2021-01-01 11:37:23 +0530

    After add action , reducers and update state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import Movies from './reducers';
+
+const store = createStore(Movies);
+console.log('store',store);
+// console.log('BEFORE STATE',store.getState());
+
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies: [{name:'Avengers'}]
+// });
+
+// console.log('AFTER STATE',store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
